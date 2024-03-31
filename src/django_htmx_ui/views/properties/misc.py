6def57730c1164d1c982a29997c1d6ac36b7a16d
Nikalexis Nikos <nikalexis@gmail.com>
2024-03-31 21:08:37 +0300

    Adds Static and Variable

--- src/django_htmx_ui/views/properties/misc.py
+++ src/django_htmx_ui/views/properties/misc.py
@@ -1,6 +1,29 @@
 from django_htmx_ui.views.properties.base import BaseProperty
 
 
+class Static(BaseProperty):
+
+    def __init__(self, value, name=None, add_in_context=False, cache=False) -> None:
+        self.value = value
+        super().__init__(name, add_in_context, cache)
+
+    def __view__(self):
+        return self.value
+
+
+class Variable(BaseProperty):
+
+    def __init__(self, value, name=None, add_in_context=False, cache=False) -> None:
+        self.value = value
+        super().__init__(name, add_in_context, cache)
+
+    def __view__(self):
+        return self.value
+
+    def _set(self, instance, value):
+        self.value = value
+
+
 class Alias(BaseProperty):
 
     def __init__(self, alias, name=None, add_in_context=True, cache=True) -> None:
@@ -14,5 +37,5 @@ class Alias(BaseProperty):
     def __view__(self):
         return getattr(self.parent, self.alias_name)
 
-    def _set(self, instance, owner, value):
+    def _set(self, instance, value):
         return setattr(instance, self.alias_name, value)
