ad49be954daf7fe97e1c5304a4c8a7ae29b1cc10
Nikalexis Nikos <nikalexis@gmail.com>
2024-03-31 21:09:19 +0300

    Adds ancestors option for Join

--- src/django_htmx_ui/views/properties/widgets/helpers.py
+++ src/django_htmx_ui/views/properties/widgets/helpers.py
@@ -6,11 +6,12 @@ from django_htmx_ui.views.properties.widgets.base import BaseWidget
 
 class Join(BaseWidget):
 
-    def __init__(self, *include_args, include=(), exclude=(), filter=bool, separator='', name=None, add_in_context=True) -> None:
+    def __init__(self, *include_args, include=(), exclude=(), filter=bool, separator='', ancestors=0, name=None, add_in_context=True) -> None:
         self.include = tuple(set(include_args) | set(include))
         self.exclude = exclude
         self.filter = filter
         self._separator = separator
+        self.ancestors = ancestors
         super().__init__(name, add_in_context)
 
     @ContextProperty
@@ -19,7 +20,10 @@ class Join(BaseWidget):
 
     @ContextCachedProperty
     def contents(self):
+        ancestor = self.parent
+        for _ in range(0, self.ancestors):
+            ancestor = ancestor.parent
         return [
-            getattr(self.parent, descriptor_name)
-            for descriptor_name, member in self.parent.get_properties(include=self.include, exclude=self.exclude, filter=self.filter)
+            getattr(ancestor, descriptor_name)
+            for descriptor_name, member in ancestor.get_properties(include=self.include, exclude=self.exclude, filter=self.filter)
         ]
