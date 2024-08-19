c1c01dd7c48193982b700597e003221a21872c7a
Nikalexis Nikos <nikalexis@gmail.com>
2024-08-19 17:34:41 +0300

    Adds more functionality into KlassDirective

--- src/django_htmx_ui/views/dominated/html_attrs.py
+++ src/django_htmx_ui/views/dominated/html_attrs.py
@@ -3,15 +3,43 @@ from django_htmx_ui.views.dominated.base import BaseDirective, BaseDominated
 
 class KlassDirective(BaseDirective):
     prefix = ''
+
+    def get_classes(self):
+        return [
+            c
+            for c in (self.current_attr() or '').split(' ')
+            if c != ''
+        ]
     
-    def add(self, *args):
+    def join_classes(self, *classes, include_current=True):
+        return " ".join(
+                self.get_classes() + list(classes) if include_current else list(classes)
+            )
+
+    def add(self, *classes):
         return self.__call__(
-            (self.current_attr() or '') + f' {" ".join(args)}'
+            self.join_classes(*classes)
         )
 
-    def __iadd__(self, other):
-        self.add(other)
-        return self
+    def __add__(self, other):
+        if type(other) is str:
+            return self.join_classes(other)
+        else:
+            return self.join_classes(*other)
+
+    def remove_classes(self, *classes):
+        return self.join_classes(*[c for c in self.get_classes() if c not in classes], include_current=False)
+
+    def remove(self, *classes):
+        return self.__call__(
+            self.remove_classes(*classes)
+        )
+
+    def __sub__(self, other):
+        if type(other) is str:
+            return self.remove_classes(other)
+        else:
+            return self.remove_classes(*other)
 
 
 class HtmlAttrsDominated(BaseDominated):
