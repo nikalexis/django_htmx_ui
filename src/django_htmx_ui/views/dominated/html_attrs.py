69f083a10da8549a96d75cacf0efe4c04cfe1eba
Nikalexis Nikos <nikalexis@gmail.com>
2024-08-21 16:41:44 +0300

    Adds StyleDirective

--- src/django_htmx_ui/views/dominated/html_attrs.py
+++ src/django_htmx_ui/views/dominated/html_attrs.py
@@ -1,4 +1,5 @@
 from django_htmx_ui.views.dominated.base import BaseDirective, BaseDominated
+import cssutils
 
 
 class KlassDirective(BaseDirective):
@@ -42,7 +43,90 @@ class KlassDirective(BaseDirective):
             return self.remove_classes(*other)
 
 
+class StyleDirective(BaseDirective):
+    prefix = ''
+    encoding='utf-8'
+    normalize = True
+
+    @property
+    def css(self):
+        return cssutils.parseStyle(self.current_attr() or '', self.encoding)
+
+    @css.setter
+    def css(self, css):
+        super().__call__(css.getCssText(' '))
+
+    def reset_style(self, **properties):
+        return self.make_style(False, **properties)
+
+    def make_style(self, __include_current=True, __replace=True, __is_pythonic_key=True, **properties):
+        css = self.css if __include_current else cssutils.css.CSSStyleDeclaration()
+
+        for name, value in properties.items():
+            value, priority = value if type(value) is tuple else (value, '')
+
+            if __is_pythonic_key:
+                name = name.replace('_', '-')
+
+            css.setProperty(name, value, priority, self.normalize, __replace)
+        
+        return css.getCssText(' ')
+    
+    def reset(self, **properties):
+        return super().__call__(
+            self.reset_style(**properties)
+        )
+
+    def add(self, __replace=True, **properties):
+        return super().__call__(
+            self.make_style(True, __replace, **properties)
+        )
+
+    def add_extra(self, **properties):
+        return self.add(False, **properties)
+
+    def __add__(self, other):
+        return self.make_style(True, True, **other)
+
+    def __setitem__(self, key, value):
+        return super().__call__(
+            self.make_style(True, True, False, **{key: value})
+        )
+
+    def __set__(self, instance, value):
+        if type(value) is dict:
+            self.__call__(**value)
+        elif type(value) is cssutils.css.CSSStyleDeclaration:
+            super().__call__(value.getCssText(' '))
+        else:
+            super().__call__(value)
+
+    def remove_properties(self, *properties):
+        css = self.css
+
+        for name in properties:
+            css.removeProperty(name, self.normalize)
+        
+        return css.getCssText(' ')
+    
+    def remove(self, *properties):
+        return super().__call__(self.remove_properties(*properties))
+
+    def __delitem__(self, key):
+        return self.remove(key)
+
+    def __sub__(self, other):
+        if type(other) is str:
+            return self.remove_properties(other)
+        else:
+            return self.remove_properties(*other)
+
+    def __call__(self, **properties):
+        super().__call__(self.reset_style(**properties))
+
+
 class HtmlAttrsDominated(BaseDominated):
     default_directive = 'klass'
 
     klass = class_ = KlassDirective()
+    style = StyleDirective()
